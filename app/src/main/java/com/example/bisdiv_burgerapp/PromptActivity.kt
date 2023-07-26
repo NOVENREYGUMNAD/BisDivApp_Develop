528d981059ce919bc015f6cb20d8b81da8150e3e
novenreygumnad <novenreygumnad@gmail.com>
2023-07-25 22:28:21 -0700

    changes

---
+++ app/src/main/java/com/example/bisdiv_burgerapp/PromptActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bisdiv_burgerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PromptActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prompt)
+    }
+}
