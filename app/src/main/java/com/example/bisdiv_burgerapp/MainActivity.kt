9abb2baaf0d147dde77b8061028eedaf972d3519
novenreygumnad <novenreygumnad@gmail.com>
2023-07-26 20:23:48 -0700

    feature_branch

--- app/src/main/java/com/example/bisdiv_burgerapp/MainActivity.kt
+++ app/src/main/java/com/example/bisdiv_burgerapp/MainActivity.kt
@@ -6,17 +6,20 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var input_FullName:EditText
-    private lateinit var input_Address:EditText
-    private lateinit var input_Number:EditText
-    private lateinit var confirm_Button:Button
+    private lateinit var input_FullName: EditText
+    private lateinit var input_Address: EditText
+    private lateinit var input_Number: EditText
+    private lateinit var confirm_Button: Button
 
-    private val db = FirebaseFirestore.getInstance()
+    private var db = FirebaseFirestore.getInstance()
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,35 +36,29 @@ class MainActivity : AppCompatActivity() {
             val inputData2 = input_Address.text.toString()
             val inputData3 = input_Number.text.toString()
 
-            if (inputData1.isNotBlank() && inputData2.isNotBlank() && inputData3.isNotBlank()){
+            val dataToSave = hashMapOf(
+                "name" to inputData1,
+                "address" to inputData2,
+                "phone" to inputData3,
+                "timestamp" to FieldValue.serverTimestamp()
+            )
 
-                sendDataToFirestore(inputData1, inputData2, inputData3)
-            }else {
-                Toast.makeText(this, "Please Enter Information", Toast.LENGTH_SHORT).show()
-            }
+            val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+            db.collection("Customer").document(userId).set(dataToSave)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Order has been submitted!", Toast.LENGTH_SHORT).show()
+
+                    input_FullName.text.clear()
+                    input_Address.text.clear()
+                    input_Number.text.clear()
+                }
+
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to submitted!", Toast.LENGTH_SHORT).show()
+                }
         }
 
     }
-
-    private fun sendDataToFirestore(name: String, address: String, phone: String) {
-
-        val collectionName = "Customer"
-
-        val documentId = "A1"
-
-        val dataToSave = hashMapOf(
-            "name" to "Noven Rey Gumnad",
-            "address" to "Tagum City",
-            "phone" to "09953652726"
-        )
-     db.collection(collectionName).document(documentId)
-
-         .set(dataToSave)
-         .addOnSuccessListener {
-             Toast.makeText(this, "Order has been sent!", Toast.LENGTH_SHORT).show()
-         }
-         .addOnFailureListener { e -> Toast.makeText(this, "Error: ${e.message}",
-         Toast.LENGTH_SHORT).show()
-         }
-    }
 }
+
